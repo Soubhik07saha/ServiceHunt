f7f2fa4bd13b53ea01eb1214a977ed7b190c6c3a
Soubhik07saha <162893486+Soubhik07saha@users.noreply.github.com>
2024-10-13 11:42:13 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import EmployeeModel from './models/Employee';
+import EmployeeModel from '../models/Employee'; // Adjust the path as necessary
 
 const app = express();
 app.use(express.json());
