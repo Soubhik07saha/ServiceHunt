cf0bc676d642d445f5c23af2e256f084c196c4e0
Soubhik07saha <162893486+Soubhik07saha@users.noreply.github.com>
2024-10-13 11:37:59 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
-const EmployeeModel = require('./models/Employee');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import EmployeeModel from './models/Employee';
 
 const app = express();
 app.use(express.json());
@@ -14,12 +14,14 @@ app.use(cors({
 }));
 
 // MongoDB connection string
-mongoose.connect("mongodb+srv://soubhiksahaetce222711:x53D-r6E%40Wz8mB8@cluster0.nw2jtsc.mongodb.net/Employee?retryWrites=true&w=majority")
+const MONGODB_URI = "mongodb+srv://soubhiksahaetce222711:x53D-r6E%40Wz8mB8@cluster0.nw2jtsc.mongodb.net/Employee?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("Error connecting to MongoDB:", err));
 
 // POST route for registering an employee
-app.post('/register', (req, res) => {
+app.post('/api/register', (req, res) => {
     EmployeeModel.create(req.body)
         .then(employee => {
             res.json({
@@ -66,7 +68,5 @@ app.get('/api/details/:city/:service', async (req, res) => {
     }
 });
 
-// Start the server
-app.listen(3001, () => {
-    console.log("Server is Running on port 3001");
-});
+// Export the app for Vercel
+export default app;
