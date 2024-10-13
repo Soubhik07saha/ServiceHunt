d3df0e1628bc88356298a50f40b7630216284e66
Soubhik07saha <162893486+Soubhik07saha@users.noreply.github.com>
2024-10-13 10:59:54 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:["https://service-hunt-proo.vercel.app/"],
+        origin:["https://service-hunt-proo.vercel.app"],
         methods:["POST", "GET"],
         credentials: true
         
